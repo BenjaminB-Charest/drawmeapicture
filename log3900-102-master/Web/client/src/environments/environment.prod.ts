8c9122ac1fec3238d73ae9aeb0fda8dbbe6e27cc
unknown <benjaminb.charest@rexys.io>
2021-04-27 23:01:30 -0400

    projet 3

---
+++ log3900-102-master/Web/client/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: 'AIzaSyASJ-MIFEiO1VcjPBvKxOotCWjLoUsD8ig',
+      authDomain: 'chat-b68d4.firebaseapp.com',
+      databaseURL: 'https://chat-b68d4-default-rtdb.firebaseio.com',
+      projectId: 'chat-b68d4',
+      storageBucket: 'chat-b68d4.appspot.com',
+      messagingSenderId: '534126703265',
+      appId: '1:534126703265:web:2378bd8e0674a0f5b336e0',
+      measurementId: 'G-6WP31201CJ',
+    },
+};
